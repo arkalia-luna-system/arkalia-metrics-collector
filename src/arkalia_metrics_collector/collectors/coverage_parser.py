bac36c955e184b3070634ff877d0fdc6eb60c954
Athalia Siwek <arkalia.luna.system@gmail.com>
2025-11-15 11:41:43 +0100

    🔒 Fix Semgrep: format nosemgrep corrigé ligne 18

--- src/arkalia_metrics_collector/collectors/coverage_parser.py
+++ src/arkalia_metrics_collector/collectors/coverage_parser.py
@@ -14,8 +14,7 @@ try:
 except ImportError:
     # Fallback si defusedxml n'est pas installé
     # nosemgrep: python.lang.security.use-defused-xml.use-defused-xml
-    # nosemgrep: python.lang.security.use-defused-xml-parse.use-defused-xml-parse
-    import xml.etree.ElementTree as ET  # noqa: S405  # nosemgrep
+    import xml.etree.ElementTree as ET  # noqa: S405  # nosemgrep: python.lang.security.use-defused-xml.use-defused-xml
 
     logging.warning(
         "defusedxml n'est pas installé. Utilisation de xml.etree.ElementTree "
